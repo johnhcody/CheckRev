614ef182d64233045f55ed8a6ef5124fd59a80a5
johnhcody <john.haner.cody@gmail.com>
2020-09-22 02:03:40 +0700

    backend routes commit

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,2 +1,24 @@
 class Api::SessionsController < ApplicationController
+
+    def create
+        @user = User.find_by_credentials(params[:user][:email], params[:user][:password])
+
+        if @user.nil?
+            render json: { message: "Incorrect username or password"}, status 401
+        else
+            login!(@user)
+            render json: { message: "Login Successful",
+                                id: @user.id }
+        end
+
+        def destroy 
+            if logged_in?
+                logout!
+                render json: {},
+            else
+                render json: { message: "Not Logged In" }, status 404
+            end
+        end
+
+    end
 end
