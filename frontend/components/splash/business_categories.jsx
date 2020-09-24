66c3a90392cbfa0331ead4099d4bf96e869ef96c
johnhcody <john.haner.cody@gmail.com>
2020-09-25 06:31:10 +0700

    working on cards

--- frontend/components/splash/business_categories.jsx
+++ frontend/components/splash/business_categories.jsx
@@ -9,18 +9,22 @@ class BusinessCategories extends React.Component {
 
     render() {
         return (
-            <div className="business-cat-container-parent">
-                <div className="heading-title">
-                    <h3>Find the Best Businesses in Town</h3>
+            <div>
+                <div className="rev-title">
+                    <h2>Find Great Businesses</h2>
                 </div>
+            <div className="business-cat-container-parent">
                 <div className="bus-cards-container">
+                <a href="#">
                     <div className="bus-card">
                         <img src={window.construction} />
                         <div className="bus-container">
-                            <h3>Massage</h3>
+                            <h3>Construction</h3>
                         </div>
                     </div>
+                </a>
                 </div>
+            </div>
             </div>
         )
     }
