838bfa9840b708a70d795b826fb79708b5502ae4
johnhcody <john.haner.cody@gmail.com>
2020-09-25 17:02:36 +0700

    featured business cards complete

--- frontend/components/splash/featured_businesses.jsx
+++ frontend/components/splash/featured_businesses.jsx
@@ -39,6 +39,50 @@ class FeaturedBusinesses extends React.Component {
                             </h4>
                         </div>
                     </div>
+                        <div className="featured-bus-card">
+                            <a href="#" >
+                                <img src={window.construction} />
+                            </a>
+                            <div>
+                                <h1>
+                                    <a href="#">Speedy Auto Repair</a>
+                                </h1>
+                                <h2>
+                                    {utensils}{utensils}{utensils} 10 reviews
+                            </h2>
+                                <h3>
+                                    business type
+                            </h3>
+                                <h3>
+                                    location
+                            </h3>
+                                <h4>
+                                    amenities
+                            </h4>
+                            </div>
+                        </div>
+                        <div className="featured-bus-card">
+                            <a href="#" >
+                                <img src={window.construction} />
+                            </a>
+                            <div>
+                                <h1>
+                                    <a href="#">Speedy Auto Repair</a>
+                                </h1>
+                                <h2>
+                                    {utensils}{utensils}{utensils} 10 reviews
+                            </h2>
+                                <h3>
+                                    business type
+                            </h3>
+                                <h3>
+                                    location
+                            </h3>
+                                <h4>
+                                    amenities
+                            </h4>
+                            </div>
+                        </div>
                 </div>
             </div>
         </div>
