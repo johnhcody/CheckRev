d564333218fd4d62f5994d33e03286dac028f325
johnhcody <john.haner.cody@gmail.com>
2020-09-28 01:27:56 +0700

    reset master key

--- app/controllers/api/businesses_controller.rb
+++ app/controllers/api/businesses_controller.rb
@@ -1,6 +1,7 @@
 class Api::BusinessesController < ApplicationController
 
     def index 
+        @businesses = Business.all
         render :index
     end
 
@@ -8,13 +9,15 @@ class Api::BusinessesController < ApplicationController
         @business = Business.create!(business_params)
     end
 
-    def business_params
-        params.require(:business).permit(:name, :address_1, :address_2, :city, :zipcode, :longitude, :latitude, :web_address, :phone_number, :business_type, :business_category)
-    end
-
+    
     def show
         @business = Business.find(params[:id])
         render :show
     end
 
+    private
+
+    def business_params
+        params.require(:business).permit(:name, :address_1, :address_2, :city, :zipcode, :longitude, :latitude, :web_address, :phone_number, :business_type, :business_category)
+    end
 end
---
+++ app/views/api/businesses/_business.json.jbuilder
@@ -0,0 +1,3 @@
+json.key_format! camelize: :lower
+
+json.extract! business, :id, :name, :address_1, :address_2, :city, :zipcode, :longitude, :web_address, :phone_number, :business_type, :business_category
