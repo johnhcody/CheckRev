30d096da8b1925357e06ef7547e5c488494fe388
johnhcody <john.haner.cody@gmail.com>
2020-09-28 22:29:56 +0700

    edited card for index page

---
+++ db/migrate/20200928125535_add_index_and_foreign_key_reviews_table.rb
@@ -0,0 +1,4 @@
+class AddIndexAndForeignKeyReviewsTable < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
--- frontend/components/businesses/business_index_item.jsx
+++ frontend/components/businesses/business_index_item.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 const PostIndexItem = props => {
-    debugger
+    const utensils = <FontAwesomeIcon icon="utensils" />
     return (
             <div className="bus-item-container">
 
@@ -13,9 +13,9 @@ const PostIndexItem = props => {
                     <div className="bus-description">
                         <div className="left-wrapper">
                             <h1>{props.name}</h1>
-                            <h2>G G G G G</h2>
-                            <h3>amenities</h3>
-                            <h4>{props.businessCategory}</h4>
+                            <h2>{utensils}{utensils}{utensils}{utensils}{utensils} 10 Reviews</h2>
+                            <h3>amenities, amenities, amenities</h3>
+                            <h4>delivery takeout outdoor seating</h4>
                         </div>
                         <div className="right-wrapper">
                             <h4>{props.phoneNumber}</h4>
