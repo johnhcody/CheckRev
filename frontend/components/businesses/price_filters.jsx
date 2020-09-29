3f3df685c761f15a9c8752251dc59a21e215725e
johnhcody <john.haner.cody@gmail.com>
2020-09-29 14:59:07 +0700

    added title

--- frontend/components/businesses/price_filters.jsx
+++ frontend/components/businesses/price_filters.jsx
@@ -8,6 +8,9 @@ class PriceFilters extends React.Component {
     render() {
         return (
             <div className="filter-placeholder">
+                <div className="filter-title">
+                    <h1>Filters</h1>
+                </div>
                 <div className="price-container">
                     <input type="radio" name="$" id="dol$"/><label id="dol1" htmlFor="dol$">$</label>
                     <input type="radio" name="$$" id="dol$$" /><label id="dol2" htmlFor="dol$$">$$</label>
