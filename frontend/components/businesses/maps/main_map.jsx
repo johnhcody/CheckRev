3d87c0137d1cd7c0ebe3d52ccf3ccbb581c40ef3
johnhcody <john.haner.cody@gmail.com>
2020-09-30 15:00:06 +0700

    fix lng lat in seeds.rb

--- frontend/components/businesses/maps/main_map.jsx
+++ frontend/components/businesses/maps/main_map.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import MarkerManager from '../../../util/marker_manager';
 
-//import MapView from 'react-native-maps';
 
 
 class MainMap extends React.Component {
@@ -17,10 +16,12 @@ class MainMap extends React.Component {
         
         this.map = new google.maps.Map(this.mapNode, mapOptions);
         this.MarkerManager = new MarkerManager(this.map);
+        this.MarkerManager.updateMarkers();
     }
     
     componentDidUpdate() {
         this.MarkerManager = new MarkerManager(this.map);
+        this.MarkerManager.updateMarkers();
     }
     render() {
         return (
@@ -31,19 +32,5 @@ class MainMap extends React.Component {
     
     
 export default MainMap;
-    
-    // <div id="map-wrapper"> 
-    //     <MapView style={StyleSheet.container}  // may need new loader? oaders are configured to process this file. See https://webpack.js.org/concepts#loaders
-    // |   const overlays = makeOverlays(props.geojson.features);
-    // |   return (
-        // > <React.Fragment>
-        //         |       {overlays.map((overlay, index) => {
-            // |         if (overlay.type === 'point') {
-                //         @ ./node_modules/react - native - maps / index.js 21:0-65 21:0-65
-                //     initialRegion={{
-                    //         latitude: 122.4194, 
-                    //         longitude: 37.7749
-                    //     }}
-                    //     />
-                    // </div>
+
                     
