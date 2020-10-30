8907f5401609f66cab15c5263ad59b8088ce29b9
johnhcody <john.haner.cody@gmail.com>
2020-10-30 16:45:54 +0700

    styling

--- frontend/components/reviews/review_form.jsx
+++ frontend/components/reviews/review_form.jsx
@@ -7,32 +7,66 @@ class ReviewForm extends React.Component {
         this.state = {
             ready: false,
             body: this.props.body,
-            busId: null,
+            busId: '',
+            rating: "0"
                     }
-
+        
         // this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     componentDidMount() {
-        this.props.fetchBusinesses()
+
     }
 
-    // handleSubmit(e) {
-    //     e.preventDefault();
-    //     this.props.action(this.state);
-    // }
+    handleSubmit(e) {
+        e.preventDefault();
+        this.props.action(this.state);
+    }
 
-    // update(field) {
-    //     return e => this.setState({ [field]: e.currentTarget.value })
-    // }
+    handleChange(field) {
+        return e => this.setState({ [field]: e.currentTarget.value })
+    }
+
+    renderRating() {
+        return <div className="radio-wrapper">
+            <label htmlFor="1">
+                <input type="radio" name="rating" id="1" value="1" onChange={this.handleChange('rating')} checked={this.state.rating == "1"} required />
+                <i className="fas fa-star"></i>
+            </label>
+            <label htmlFor="2">
+                <input type="radio" name="rating" id="2" value="2" onChange={this.handleChange('rating')} checked={this.state.rating == "2"} required />
+                <i className="fas fa-star"></i>
+            </label>
+            <label htmlFor="3">
+                <input type="radio" name="rating" id="3" value="3" onChange={this.handleChange('rating')} checked={this.state.rating == "3"} required />
+                <i className="fas fa-star"></i>
+            </label>
+            <label htmlFor="4">
+                <input type="radio" name="rating" id="4" value="4" onChange={this.handleChange('rating')} checked={this.state.rating == "4"} required />
+                <i className="fas fa-star"></i>
+            </label>
+            <label htmlFor="5">
+                <input type="radio" name="rating" id="5" value="5" onChange={this.handleChange('rating')} checked={this.state.rating == "5"} required />
+                <i className="fas fa-star"></i>
+            </label>
+        </div>
+    }
 
     render() {
         const business = this.props.allBusinesses[parseInt(window.location.href.split('/')[window.location.href.split('/').length - 2])]
-        debugger
             return (
                 <div className="review-form-wrapper">
                     <div className="review-form">
                         <h1>{business.name}</h1>
+                        <form onSubmit={this.handleSubmit}>
+                            <div className="rating-and-text-wrapper">
+                                <div className="rating-placeholder"></div>
+                                <div className="textarea-wrapper">
+                                    <textarea name="" id=""></textarea>
+                                </div>
+                            </div>
+                            <button>Post Review</button>
+                        </form>
                     </div>
                 </div>
             )
