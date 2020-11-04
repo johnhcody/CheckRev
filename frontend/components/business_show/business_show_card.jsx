991c3ddafcd491c3603c8aab0bbf4da6ad0cda71
johnhcody <john.haner.cody@gmail.com>
2020-11-04 10:32:19 +0700

    rendering average rating icons

--- frontend/components/business_show/business_show_card.jsx
+++ frontend/components/business_show/business_show_card.jsx
@@ -10,6 +10,7 @@ class BusinessShowCard extends React.Component {
             display: "none"
         };
         this.checkCurrentUser = this.checkCurrentUser.bind(this);
+        this.renderAverageRating = this.renderAverageRating.bind(this);
     }
     
     checkCurrentUser (e) {
@@ -20,6 +21,39 @@ class BusinessShowCard extends React.Component {
             debugger
             this.props.history.push(`/businesses/${this.props.id}/review-new`)
             //return <Redirect to={`/businesses/${this.props.id}/review-new`} />
+        }
+    }
+
+    renderAverageRating() {
+        const len = this.props.reviews.length;
+        let total = 0;
+        this.props.reviews.forEach(review => {
+            total += review.rating;
+        })
+        const halfStar = window.halfStar;
+        const fullStar = window.fullStar;
+        const avg = (total / len).toFixed(1)
+
+        if (avg > 0 && avg < 1) {
+            return <img src={halfStar}/>
+        } else if (avg >= 1 && avg <= 1.4) {
+            return <img src={fullStar} />
+        } else if (avg >= 1.5 && avg < 2) {
+            return <div><img src={fullStar} /> <img src={halfStar}/></div> 
+        } else if (avg >= 2 && avg <= 2.4 ) {
+            return <div><img src={fullStar} /> <img src={fullStar} /></div>
+        } else if (avg >= 2.5 && avg < 3 ) {
+            return <div><img src={fullStar} /> <img src={fullStar} /> <img src={halfStar} /></div>
+        } else if (avg >= 3 && avg <= 3.4 ) {
+            return <div><img src={fullStar} /> <img src={fullStar} /> <img src={fullStar} /></div>
+        } else if (avg >= 3.5 && avg < 4 ) {
+            return <div><img src={fullStar} /> <img src={fullStar} /> <img src={fullStar} /> <img src={halfStar} /></div>
+        } else if (avg >= 4 && avg <= 4.4 ) {
+            return <div><img src={fullStar} /> <img src={fullStar} /> <img src={fullStar} /> <img src={fullStar} /></div>
+        } else if (avg > 4.4 && avg <= 4.6 ) {
+            return <div><img src={fullStar} /> <img src={fullStar} /> <img src={fullStar} /> <img src={fullStar} /> <img src={halfStar} /></div>
+        } else if (avg > 4.6) {
+            return <div><img src={fullStar} /> <img src={fullStar} /> <img src={fullStar} /> <img src={fullStar} /> <img src={fullStar} /></div>
         }
     }
     
@@ -35,7 +69,7 @@ class BusinessShowCard extends React.Component {
                     <h1>{this.props.name}</h1>
                     <div>
                         <div className="bus-show-rating">
-                            <a>{star} {star} {star} {star} {star} </a> 
+                            {this.renderAverageRating()} 
                             <h2>{this.props.reviews.length.toString()} Reviews</h2>
                         </div>
                         <div className="bus-show-review">
